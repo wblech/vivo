bc40620c21a52b6c7239588c5a648f356c69f97a
Wincenty Bertoni Lech <wblech@gmail.com>
2020-12-01 22:00:38 -0300

    Refactor function to use generator comprehension

--- vivo/get_an.py
+++ vivo/get_an.py
@@ -10,9 +10,7 @@ logger.setLevel(settings.LOG_LEVEL)
 
 def get_an(num_list):
     try:
-        record = {}
-        for expected_num in range(16):
-            record[expected_num] = 0
+        record = {expected_num: 0 for expected_num in range(16)}
         for num in num_list:
             if num < 0 or num > 15:
                 raise Exception('Number should be between 0 and 15')
