702295afc9e7f2ae18d67df0f287ebd3f5f37660
Wincenty Bertoni Lech <wblech@gmail.com>
2020-12-06 09:50:35 -0300

    ADD docstring at get_an func

--- vivo/ex01/get_an.py
+++ vivo/ex01/get_an.py
@@ -9,6 +9,14 @@ logger.setLevel(settings.LOG_LEVEL)
 
 
 def get_an(num_list):
+    """Algoritmo que receba como entrada um Vetor A[N] em que An ∈ { 0, 1, 2,..., 15 }.
+        Devolve uma String indicando a quantidade de vezes que cada An foi encontrado na matriz de bitmap. No caso em que algum elemento não tenha sido encontrado, o algoritmo deve retornar que a quantidade é zero para aquele elemento.
+    Args:
+        num_list (list): Lista de inteiros do 0 ao 15
+
+    Returns:
+        retorna uma string em formato de dicionário com a indicação de quantas vezes cada número aparece na lista
+    """
     try:
         record = {expected_num: 0 for expected_num in range(16)}
         for num in num_list:
